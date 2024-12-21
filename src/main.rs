3fd6c545cd0570d5adb8f73555793b5e81fc9e62
tsuphona <thomas.suphona@gmail.com>
2024-12-21 18:35:27 +0100

    Add server functions

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use env_logger;
 
 static CSS: Asset = asset!("/assets/main.css");
 
+// Create a new wrapper type.
+#[derive(Clone)]
+struct TitleState(String);
+
+#[derive(serde::Deserialize)]
+struct DogApi {
+	message: String,
+}
+
+
 fn main() {
     env_logger::init();
     dioxus::launch(App);
@@ -10,6 +20,8 @@ fn main() {
 
 #[component]
 fn App() -> Element {
+    // Provide that type as a Context
+    use_context_provider(|| TitleState("HotDog".to_string()));
     rsx! {
         document::Stylesheet { href: CSS }
         Title {}
@@ -19,26 +31,62 @@ fn App() -> Element {
 
 #[component]
 fn Title() -> Element {
+    // Consume that type as a Context
+    let title = use_context::<TitleState>();
     rsx! {
-        div { id: "title",
-            h1 { "HotDog! 🌭" }
-        }
+        h1 { "{title.0}" }
     }
 }
 
 #[component]
 fn DogView() -> Element {
-    let skip = move |evt| {};
-    let save = move |evt| {};
-    let img_src = use_hook(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
 
     rsx! {
         div { id: "dogview",
-            img { src: "{img_src}" }
+            img { src: img_src.cloned().unwrap_or_default() }
         }
         div { id: "buttons",
-            button { onclick: skip, id: "skip",  "skip" }
-            button { onclick: save, id: "save",  "save!" }
+             button {
+                onclick: move |_| async move {
+                    // Clone the current image
+                    let current = img_src.cloned().unwrap();
+
+                    // Start fetching a new image
+                    img_src.restart();
+
+                    // And call the `save_dog` server function
+                    save_dog(current).await;
+                },
+                "save!"
+            }
         }
     }
+}
+
+// Expose a `save_dog` endpoint on our server that takes an "image" parameter
+#[server]
+async fn save_dog(image: String) -> Result<(), ServerFnError> {
+    use std::io::Write;
+
+    // Open the `dogs.txt` file in append-only mode, creating it if it doesn't exist;
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("dogs.txt")
+        .unwrap();
+
+    // And then write a newline to it with the image url
+    file.write_fmt(format_args!("{image}\n"));
+
+    Ok(())
 }
