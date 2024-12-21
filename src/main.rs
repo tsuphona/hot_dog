66e1497b3bdc74500b355ca617e11a2b1fe546fe
tsuphona <thomas.suphona@gmail.com>
2024-12-21 16:06:00 +0100

    Add CSS

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 use dioxus::prelude::*;
+use env_logger;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
+static CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
+    env_logger::init();
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    rsx! { "Hot Dog!" }
+    rsx! {
+        Title {}
+        DogView {}
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
+        div { id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    let skip = move |evt| {};
+    let save = move |evt| {};
+    let img_src = use_hook(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
+
+    rsx! {
+        div { id: "dogview",
+            img { src: "{img_src}" }
+        }
+        div { id: "buttons",
+            button { onclick: skip, id: "skip",  "skip" }
+            button { onclick: save, id: "save",  "save!" }
+        }
+    }
 }
